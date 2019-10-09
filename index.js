4f7f295daf390063b5a53b896d96a93948b5ac7d
Fabien Aubret <fabien.aubret@gmail.com>
2019-10-09 14:58:55 +0200

    [Add] Add "le-gala" page

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ app.get('/comment-venir/', (req, res) => {
     res.render('comment-venir.html.twig');
 });
 
+app.get('/le-gala/', (req, res) => {
+    res.render('le-gala.html.twig');
+});
+
+app.get('/countup.min.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/countup.js/dist/countup.min.js');
+});
+
 app.listen(1234);
